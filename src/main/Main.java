2e6e84f6e61c2d2b075a284abb270efc70237eee
Pavel Václavek <pavel.vaclavek88@gmail.com>
2025-04-02 18:08:09 +0200

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+}
