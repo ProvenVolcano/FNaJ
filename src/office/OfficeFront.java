a379658d4e8521e3adbcecd38bfdcf8719a2b6ea
Pavel Václavek <pavel.vaclavek88@gmail.com>
2025-05-29 20:42:38 +0200

    Added closing doors and window animation

--- src/office/OfficeFront.java
+++ src/office/OfficeFront.java
@@ -2,10 +2,15 @@ package office;
 
 import cameras.Camera;
 import info.*;
+import javafx.animation.TranslateTransition;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import cameras.Monitor;
+import javafx.util.Duration;
 
 /**
  * Class of the front of the office
@@ -14,6 +19,10 @@ public class OfficeFront extends OfficeTemplate {
 
     private Button monitorButton;
     private Button left, right;
+    private Rectangle bgBlack;
+    private ImageView door;
+    private TranslateTransition closeMove;
+    private TranslateTransition openMove;
 
     public OfficeFront(Stage stage, int width, int height, OfficeLeft officeLeft, OfficeRight officeRight, Monitor monitor, InfoPane info, InfoProperties ip, Camera cam) {
         super(info, "res/office/officeFront.png", width, height, cam);
@@ -56,25 +65,50 @@ public class OfficeFront extends OfficeTemplate {
             stage.setScene(officeRight.getScene());
         });
 
+        bgBlack = new Rectangle(900, 400);
+        bgBlack.setFill(Color.BLACK);
+
+        door = new ImageView("file:res/office/officeFrontDoor.png");
+        AnchorPane.setLeftAnchor(door, 585.0);
+        AnchorPane.setTopAnchor(door, 235.0 - 92);
+
         doorButton.setPrefWidth(190);
         doorButton.setPrefHeight(110);
         doorButton.setLayoutX(573);
         doorButton.setLayoutY(225);
         doorButton.setOpacity(0);
 
+        closeMove = new TranslateTransition(new Duration(150), door);
+        closeMove.setByY(92);
+        closeMove.setOnFinished(e -> {
+            doorButton.setDisable(false);
+        });
+
+        openMove = new TranslateTransition(new Duration(150), door);
+        openMove.setByY(-92);
+        openMove.setOnFinished(e -> {
+            doorButton.setDisable(false);
+        });
+
         doorButton.setOnAction(e -> {
             if(ip.getPower() > 0) {
+                doorButton.setDisable(true);
+
                 if (!neighbourCam.isClosed()) {
+                    closeMove.play();
                     ip.increaseUsage();
-                } else ip.decreaseUsage();
+                } else {
+                    openMove.play();
+                    ip.decreaseUsage();
+                }
 
                 neighbourCam.setClosed(!neighbourCam.isClosed());
             }
         });
 
-        root.getChildren().add(monitorButton);
-        root.getChildren().add(left);
-        root.getChildren().add(right);
+        root.getChildren().addAll(bgBlack, monitorButton, left, right, door);
+        door.toBack();
+        bgBlack.toBack();
     }
 
     /**
--- src/office/OfficeLeft.java
+++ src/office/OfficeLeft.java
@@ -3,13 +3,23 @@ package office;
 import cameras.Camera;
 import info.InfoPane;
 import info.InfoProperties;
+import javafx.animation.TranslateTransition;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 /**
  * Class for the left side of the office
  */
 public class OfficeLeft extends OfficeTemplate {
+
+    private Rectangle bgBlack;
+    private ImageView door;
+    private TranslateTransition closeMove;
+    private TranslateTransition openMove;
 
     public OfficeLeft(int width, int height, Button backButton, InfoPane info, InfoProperties ip, Camera cam) {
         super(info, "res/office/officeLeft.png", width, height, cam);
@@ -24,15 +34,44 @@ public class OfficeLeft extends OfficeTemplate {
         doorButton.setLayoutY(130);
         doorButton.setOpacity(0.0);
 
+        bgBlack = new Rectangle(1200, 900);
+        bgBlack.setFill(Color.BLACK);
+
+        door = new ImageView("file:res/office/officeLeftDoor.png");
+        AnchorPane.setLeftAnchor(door, 600.0);
+        AnchorPane.setTopAnchor(door, 155.0 - 737);
+
+        closeMove = new TranslateTransition(new Duration(200), door);
+        closeMove.setByY(737);
+        closeMove.setOnFinished(e -> {
+            doorButton.setDisable(false);
+        });
+
+        openMove = new TranslateTransition(new Duration(200), door);
+        openMove.setByY(-737);
+        openMove.setOnFinished(e -> {
+            doorButton.setDisable(false);
+        });
+
         doorButton.setOnAction(e -> {
             if(ip.getPower() > 0) {
+                doorButton.setDisable(true);
+
                 if (!neighbourCam.isClosed()) {
+                    closeMove.play();
                     ip.increaseUsage();
-                } else ip.decreaseUsage();
+                } else {
+                    openMove.play();
+                    ip.decreaseUsage();
+                }
 
                 neighbourCam.setClosed(!neighbourCam.isClosed());
             }
         });
+
+        root.getChildren().addAll(bgBlack, door);
+        door.toBack();
+        bgBlack.toBack();
     }
 
     /**
